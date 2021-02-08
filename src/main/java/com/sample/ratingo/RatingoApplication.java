513523632d1eed706c4cad31cf57e72a98805e8b
Rajanish <rajanishatwork@gmail.com>
2021-02-09 04:29:08 +0530

    initial commit
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

---
+++ src/main/java/com/sample/ratingo/RatingoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.ratingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/ratingo/RatingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.ratingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
